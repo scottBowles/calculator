b3cc1ff796c88a8ea69c6af2e02b24930a270a9b
Scott Bowles <shbowles@gmail.com>
2020-02-21 21:48:52 -0500

    Create calculator with basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# calculator
+Another project with The Odin Project https://www.theodinproject.com/courses/web-development-101/lessons/calculator
+
---
+++ script.js
@@ -0,0 +1,121 @@
+function add (a, b=0) {
+  return a + b;
+}
+
+function subtract (a, b=0) {
+  return a - b;
+}
+
+function multiply (a, b=1) {
+  return a * b;
+}
+
+function divide (a, b=1) {
+  return a / b;
+}
+
+function percent(a, b=1) {
+  return (a / 100) * b;
+}
+
+const operationFunctions = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+  "%": percent,
+}
+
+function operate () {
+  secondOperand = workingEntry;
+  let result = operationFunctions[operator](+firstOperand, (secondOperand ? +secondOperand : undefined));
+  firstOperand = String(result);
+  workingEntry = undefined;
+  secondOperand = undefined;
+  operator = undefined;
+  display.textContent = firstOperand;
+}
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach( button => 
+  button.addEventListener("click", () => 
+    newInput(button.textContent))
+);
+
+
+const display = document.querySelector("#display");
+
+display.textContent = "0";
+let workingEntry = undefined;
+let firstOperand = undefined;
+let secondOperand = undefined;
+let operator = undefined;
+
+function newInput(input){
+  switch (input) {
+    case (!isNaN(+input) ? input : null):
+      workingEntry ? workingEntry += input : workingEntry = input;
+      display.textContent = workingEntry;
+      break;
+    case "/": 
+    case "*": 
+    case "-": 
+    case "+":  
+    case "%": 
+      if (operator) {
+        operate();
+      }
+      else {
+        if (workingEntry) {
+          firstOperand = workingEntry;
+          workingEntry = undefined;
+        }
+      }
+      display.textContent += input;
+      operator = input;
+      break;
+    case ".":
+      if (!workingEntry) {
+        workingEntry = "0.";
+        display.textContent = workingEntry;
+      }
+      else if (!workingEntry.includes(".")) {
+        workingEntry += ".";
+        display.textContent = workingEntry;
+      }
+      break;
+    case "+/-":
+      if (workingEntry) {
+        workingEntry = String(+workingEntry * -1);
+        display.textContent = workingEntry;
+      }
+      else if (firstOperand) {
+        firstOperand = String(+firstOperand * -1);
+        display.textContent = firstOperand;
+      }
+      break;
+    case "=":
+      if (operator) {
+        operate();
+      }
+      else {
+        firstOperand = workingEntry;
+        workingEntry = undefined;
+      }
+      break;
+    case "Clear":
+      display.textContent = firstOperand || "0";
+      workingEntry = undefined;
+      break;
+    case "Clear All":
+      display.textContent = "0"
+      workingEntry = undefined;
+      firstOperand = undefined;
+      secondOperand = undefined;
+      operator = undefined;
+      break;
+    default:
+      console.log("Something went wrong");
+  }
+}
